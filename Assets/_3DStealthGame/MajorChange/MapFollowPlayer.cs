9a59bfde35e10df2839a79ab05bcf6f65a437d98
tmtatiana <taleenamwork@gmail.com>
2025-12-02 00:45:08 -0500

    Minimap Benchmark

---
+++ Assets/_3DStealthGame/MajorChange/MapFollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MapFollowPlayer : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    private Image mapPosition;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
